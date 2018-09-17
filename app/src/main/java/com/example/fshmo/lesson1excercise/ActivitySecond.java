87ac5bb2c6f7c5066540792a9b3dcaf2bacd3f61
the_maker <fshmorgilov@gmail.com>
2018-09-17 16:38:17 +0300

    Initial

---
+++ app/src/main/java/com/example/fshmo/lesson1excercise/ActivitySecond.java
@@ -0,0 +1,55 @@
+package com.example.fshmo.lesson1excercise;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+public class ActivitySecond extends AppCompatActivity {
+
+    private static final String LTAG = ActivitySecond.class.getName();
+    public static final String KEY_TEXT = "KEY_TEXT";
+    private TextView textView;
+    private Activity activity = this;
+    private Button sendEmailBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        textView = findViewById(R.id.text_view);
+        textView.setText(getIntent().getStringExtra(KEY_TEXT));
+        sendEmailBtn = findViewById(R.id.sendEmailBtn);
+        sendEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] addresses = new String[]{"fshmorgilov@gmail.com"};
+                composeEmail(addresses, "Bug report", getIntent().getStringExtra(KEY_TEXT));
+            }
+        });
+
+
+    }
+
+    public static void start(Activity activity, String text){
+        Intent intent = new Intent(activity, ActivitySecond.class);
+        intent.putExtra(KEY_TEXT, text);
+        activity.startActivity(intent);
+    }
+
+    public void composeEmail(String[] addresses, String subject, String body) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+}
+
---
+++ app/src/main/java/com/example/fshmo/lesson1excercise/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.fshmo.lesson1excercise;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LTAG = MainActivity.class.getName();
+
+    private Button previewBtn;
+    private EditText editText;
+    private Activity currentActivity = this;
+//    public static final String KEY_TEXT = "KEY_TEXT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        previewBtn = findViewById(R.id.previewBtn);
+
+        if (editText.getText() != null)
+            previewBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i(LTAG, "BTN PRESSED");
+                    String entered = editText.getText().toString();
+                    Log.i(LTAG, entered);
+                    System.out.println(entered);
+                    Toast.makeText(currentActivity, "Showing your text", Toast.LENGTH_LONG).show();
+                    ActivitySecond.start(currentActivity, entered);
+
+                }
+            });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
